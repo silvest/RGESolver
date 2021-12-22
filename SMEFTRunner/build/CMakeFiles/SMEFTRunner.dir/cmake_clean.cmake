d25b7a1916aa42e37054ba7f8e0da567da51fe5a
StefanoDiNoi <67732345+StefanoDiNoi@users.noreply.github.com>
2021-12-22 14:09:15 +0100

    Add files via upload

---
+++ SMEFTRunner/build/CMakeFiles/SMEFTRunner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SMEFTRunner.dir/RGESolver.cc.o"
+  "libSMEFTRunner.a"
+  "libSMEFTRunner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SMEFTRunner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
