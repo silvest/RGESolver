9d40f22388ded225c0b4a5f4c69341390219ffa3
StefanoDiNoi <stefano.dinoi@studenti.unipd.it>
2022-06-24 23:53:07 +0200

    Updates in the documentation

--- Examples/ExampleBackRotation.cpp
+++ Examples/ExampleBackRotation.cpp
@@ -194,10 +194,12 @@ int main() {
             << S.GetCoefficient("Cqu1R", 1, 2, 2, 2) << std::endl;
     std::cout << "CllR(1,1,1,2)(" << muLow << " GeV) : "
             << S.GetCoefficient("CllR", 1, 1, 1, 2) << std::endl;
-    
-    
+
+
     //Mass of the fermions: 
     std::cout << "Up quark mass at " << muLow << " GeV : "
-            << S.GetCoefficient("mu") << std::endl;
+            << S.GetFermionMass("mu") << std::endl;
+    std::cout << "Sin theta12 " << muLow << " GeV : "
+            << S.GetCKMAngle("s12") << std::endl;
     return 0;
 }
